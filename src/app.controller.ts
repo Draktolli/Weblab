0bdab6871d4814fa739e31186e85301ed75268a3
Tolya <t.koverin@gmail.com>
2022-03-25 20:56:59 +0300

    lab3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -21,4 +21,44 @@ export class AppController {
   asda() {
     return { user: 'Ку молодой' }; // Модель представления
   }
+  @Get('/Arcanas')
+  @Render('Arcanas') // <= Название вашего представления
+  login2() {
+    return { user: 'Ку молодой' }; // Модель представления
+  }
+  @Get('/Comments')
+  @Render('Comments') // <= Название вашего представления
+  login3() {
+    return { user: 'Ку молодой' }; // Модель представления
+  }
+  @Get('/Garant')
+  @Render('Garant') // <= Название вашего представления
+  login4() {
+    return { user: 'Ку молодой' }; // Модель представления
+  }
+  @Get('/Immortals')
+  @Render('Immortals') // <= Название вашего представления
+  login5() {
+    return { user: 'Ку молодой' }; // Модель представления
+  }
+  @Get('/Legendaries')
+  @Render('Legendaries') // <= Название вашего представления
+  login6() {
+    return { user: 'Ку молодой' }; // Модель представления
+  }
+  @Get('/Questions_and_Answers')
+  @Render('Questions_and_Answers') // <= Название вашего представления
+  login7() {
+    return { user: 'Ку молодой' }; // Модель представления
+  }
+  @Get('/Sets')
+  @Render('Sets') // <= Название вашего представления
+  login8() {
+    return { user: 'Ку молодой' }; // Модель представления
+  }
+  @Get('/Suggestions')
+  @Render('Suggestions') // <= Название вашего представления
+  login9() {
+    return { user: 'Ку молодой' }; // Модель представления
+  }
 }
